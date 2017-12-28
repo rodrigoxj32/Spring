dc4c6d898c8ac0d00cd8683a579b902d50e16321
Rodrigo <rodrigoxj32@hotmail.com>
2017-12-28 13:15:01 -0600

    creando un repository

---
+++ src/main/java/com/rodrigo/repository/CursoJpaRepository.java
@@ -0,0 +1,25 @@
+package com.rodrigo.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rodrigo.entity.Curso;
+
+@Repository("cursoJpaRepository")
+public interface CursoJpaRepository extends JpaRepository<Curso, Serializable>{
+
+	/* Todas son consultas el findBy ya es reservado y detecta automaticamente los atributos de la clase
+	public abstract Curso findByPrecio(int precio);
+	
+	public abstract Curso findByPrecioAndNombre(int precio, String nombre);
+	
+	public abstract List<Curso> findByNombreOrderByHoras(String nombre);
+	
+	public abstract Curso findByNombreOrPrecio(String nombre, int precio);*/
+	
+	
+	
+}
